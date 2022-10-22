0db620b827141412f751abf0d4717d66eb91c422
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-10-22 20:28:56 +0900

    5:11 / 5:15 45. Reactでのイベントやstyleの扱い方を知る

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert("onClickButton");
+  const contentStyle = {
+    color: "blue"
+  };
   return (
     <>
-      <h1>Appこん</h1>
+      {/* その側の{}はJavaScriptを書く宣言、内側の{}はJSONオブジェクト */}
+      <h1 style={{ color: "red" }}>Appこん</h1>
+      <p style={contentStyle}>あお</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
