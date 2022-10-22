0a5cc9740c204288dc658f869174804a14b610c4
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-10-22 23:15:03 +0900

    47. Stateを知る(useState)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage"
 
 const App = () => {
-  const onClickButton = () => alert("onClickButton");
+  const onClickCountUp = () => {
+setNum(num + 1);
+  };
+  const [num, setNum] = useState(0);
   
   return (
     <>
       {/* その側の{}はJavaScriptを書く宣言、内側の{}はJSONオブジェクト */}
       <h1 style={{ color: "red" }}>Appこん</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ！</button>
+      <p>{num}</p>
     </>
   );
 };
